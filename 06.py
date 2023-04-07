2582d664b1720f58fa82604bc1ef0ac7318b7ca9
EC2 Default User <ec2-user@ip-172-31-77-45.ec2.internal>
2023-04-07 00:01:24 +0000

    listapyt

---
+++ 06.py
@@ -0,0 +1,20 @@
+
+idade = int(input("Digite sua idade: "))
+categoria = input("Digite sua categoria (estudante, aposentado, etc.): ")
+dia_semana = input("Digite o dia da semana (segunda, terça, etc.): ")
+
+desconto = 0
+
+if idade >= 60:
+    desconto = 0.3
+elif categoria == "estudante" and (dia_semana == "segunda" or dia_semana == "terça"):
+    desconto = 0.2
+elif categoria == "aposentado":
+    desconto = 0.15
+elif categoria == "professor" and dia_semana == "quarta":
+    desconto = 0.25
+
+if desconto > 0:
+    print("Você tem direito a um desconto de {:.0%}.".format(desconto))
+else:
+    print("Você não tem direito a nenhum desconto.")
